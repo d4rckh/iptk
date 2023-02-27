aeb38085ab2e8a2561ec0d27fa69a89f81e06e90
d4rckh <35298550+d4rckh@users.noreply.github.com>
2023-02-27 23:10:12 +0200

    vlsm

--- readme.md
+++ readme.md
@@ -1,12 +1,12 @@
 # iptk - ip toolkit
 
-## building
+## build
 ```bash
 cargo build
 ```
 ## usage
 ```bash
-ipcalc ip4 192.168.1.1 255.255.255.0
+iptk ip4 192.168.1.1 255.255.255.0
 
 # output:
 
@@ -16,5 +16,26 @@ ipcalc ip4 192.168.1.1 255.255.255.0
 # Network Id:  192.168.1.0
 # Broadcast:   192.168.1.255
 # Hosts:       254
+```
+```bash
+iptk vlsm 10.0.0.0 255.255.0.0 100 200 300 400
 
+# output:
+
+# IP:   10.0.0.0
+# Mask: 255.255.0.0
+# -- Network Information --
+# Network Id:  10.0.0.0
+# Broadcast:   10.0.255.255
+# Hosts:       65534
+# -- VLSM --
+# Needed Size: 400; Actual Size: 510; 10.0.0.0/23 => 10.0.1.255/23
+# Needed Size: 300; Actual Size: 510; 10.0.2.0/23 => 10.0.3.255/23
+# Needed Size: 200; Actual Size: 254; 10.0.4.0/24 => 10.0.4.255/24
+# Needed Size: 100; Actual Size: 126; 10.0.5.0/25 => 10.0.5.127/25
+# -- VLSM Stats --
+# Total Needed Hosts: 1000
+# Max Hosts Available: 1400
+# % subnet used: 71.43%
+# % network used: 1.53%
 ```
--- src/main.rs
+++ src/main.rs
@@ -18,22 +18,37 @@ fn dec_to_ip4(dec: u32) -> String {
     .iter().map(|c| c.to_string()).collect::<Vec<String>>().join(".")
 }
 
+fn get_power2(n: u32) -> u32 {
+  let mut e = 0;
+
+  while n > u32::pow(2, e) {
+    e += 1;
+  }
+
+  e
+}
+
 fn main() {
-  let command = env::args().nth(1).expect("Provide a command");
 
-  if command == "ip4" {
-    
-    let ip4 = env::args().nth(2).expect("Provide an IP address.");
-    let mask = env::args().nth(3).expect("Provide a network mask.");
+  let mut args = env::args();
 
-    let ipdec = parse_ip4(ip4.as_str());
-    let maskdec = parse_ip4(mask.as_str());
+  args.next(); // skip first arg
 
-    let niddec = ipdec & maskdec;
-    let broadcastdec = ipdec | !maskdec;
-    let nid = dec_to_ip4(niddec);
-    let broadcast = dec_to_ip4(broadcastdec);
-    let hosts = broadcastdec - niddec - 1; 
+  let command = args.next().expect("Provide a command");
+
+  let ip4 = args.next().expect("Provide an IP address.");
+  let mask = args.next().expect("Provide a network mask.");
+
+  let ipdec = parse_ip4(ip4.as_str());
+  let maskdec = parse_ip4(mask.as_str());
+
+  let niddec = ipdec & maskdec;
+  let broadcastdec = ipdec | !maskdec;
+  let nid = dec_to_ip4(niddec);
+  let broadcast = dec_to_ip4(broadcastdec);
+  let hosts = broadcastdec - niddec - 1; 
+
+  if command == "ip4" || command == "vlsm" {
 
     println!("IP:   {ip4}");
     println!("Mask: {mask}");
@@ -42,5 +57,53 @@ fn main() {
     println!("Network Id:  {nid}");
     println!("Broadcast:   {broadcast}");
     println!("Hosts:       {hosts}");
+  
+  } 
+  
+  if command == "vlsm" {
+    
+    println!("-- VLSM --");
+
+    let mut total_needed_hosts: u32 = 0;
+    let mut max_hosts: u32 = 0;
+
+    let mut vlsm_sizes = args.map(|x| x.parse::<u32>().unwrap()).collect::<Vec<u32>>();
+
+    vlsm_sizes.sort();
+    vlsm_sizes.reverse();
+
+    let mut last_niddec = niddec;
+
+    for vlsm_size in vlsm_sizes {
+      let mask = 32 - get_power2(vlsm_size + 2);
+      let shosts = u32::pow(2, 32 - mask) - 2;
+      let niddec = last_niddec;
+      let broadcastdec = niddec + shosts + 1;
+      let nid = dec_to_ip4(niddec);
+      let bc = dec_to_ip4(broadcastdec);
+
+      if max_hosts + shosts > hosts {
+        println!("-> couldn't finish subnetting: not enough space");
+        break;
+      }
+
+      total_needed_hosts += vlsm_size;
+      max_hosts += shosts;
+
+      println!("Needed Size: {vlsm_size}; Actual Size: {shosts}; {nid}/{mask} => {bc}/{mask}");
+
+      last_niddec = broadcastdec + 1;
+    }
+
+    println!("-- VLSM Stats --");
+
+    let p_susage: f64 = total_needed_hosts as f64 / max_hosts as f64 * 100.0;
+    let p_nusage: f64 = total_needed_hosts as f64 / hosts as f64 * 100.0;
+
+    println!("Total Needed Hosts: {total_needed_hosts}");
+    println!("Max Hosts Available: {max_hosts}");
+    println!("% subnet used: {:.1$}%", p_susage, 2);
+    println!("% network used: {:.1$}%", p_nusage, 2);
+
   }
 }
